6ff4edefd537de474a43027392c036964eab30db
YAMAKI-Kengo <c1318809@st.kanazawa-it.ac.jp>
2024-03-12 00:25:50 +0900

    add:componentsディレクトリの作成とheader

---
+++ front/app/components/Header.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="h-16 w-full flex items-center px-4">
+      <div className="order-first">
+        <button className="w-12 h-12 rounded-full hover:bg-gray-200 flex items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="black"
+            className="bi bi-list"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="flex items-center ml-4">
+        <img src="/Q.png" alt="Q" className="w-8 h-8" />
+        <p className="text-3xl ml-4">Q-ON!</p>
+        <button className="w-16 h-12 rounded-lg border border-gray-400 hover:bg-gray-200 ml-8 flex items-center justify-center text-base">
+          今日
+        </button>
+      </div>
+      <button className="w-12 h-12 rounded-full hover:bg-gray-200 ml-8 flex items-center justify-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="currentColor"
+          className="bi bi-chevron-left"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+          />
+        </svg>
+      </button>
+      <button className="w-12 h-12 rounded-full hover:bg-gray-200 flex items-center justify-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="currentColor"
+          className="bi bi-chevron-right"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+          />
+        </svg>
+      </button>
+      <p className="text-lg ml-8">2024年3月12日</p>
+    </header>
+  );
+};
+
+export default Header;
