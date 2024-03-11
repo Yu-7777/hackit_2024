f17ffb1388eb62bd0ea7e2bf357e237c37105481
Yu-7777 <c1306491@st.kanazawa-it.ac.jp>
2024-03-12 03:05:32 +0900

    Userコントローラー調整

--- back/app/controllers/users_controller.rb
+++ back/app/controllers/users_controller.rb
@@ -1,16 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show update destroy ]
 
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
+  def new
+    @user = User.new
   end
 
   # POST /users
