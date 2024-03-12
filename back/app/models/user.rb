2dc3f58b4a76ee46b65fef564d19ca2bf3681fba
Your Name <you@example.com>
2024-03-12 23:34:52 +0900

    userモデルのジェネレート

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   before_save { email.downcase! }
   EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
