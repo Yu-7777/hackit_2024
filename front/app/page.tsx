cb45da86586f255793a0b8ce79b56dd41f0a3687
elca-hub <csubaru07@gmail.com>
2024-03-13 14:48:00 +0900

    カレンダーの表示形式を変更

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,13 +1,16 @@
+'use client'
+
 import FullCalendar from "@fullcalendar/react";
 import Header from "./components/Header";
 import SideMenu from "./components/Sidemenu";
+import dayGridPlugin from "@fullcalendar/daygrid"; // pluginは、あとから
 
 const Page = () => {
   return (
     <>
       <Header></Header>
       <SideMenu></SideMenu>
-      <FullCalendar></FullCalendar>
+      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
     </>
   );
 };
