1bdfb2dc5ab4cce6d7ba952a7b61a82660c999f1
elca-hub <csubaru07@gmail.com>
2024-03-14 12:32:23 +0900

    作成してもらった箇所のコメントアウト

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ApplicationRecord
         
-            include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
 
-  before_save { email.downcase! }
-  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence:   true,
-                    length:     { maximum: 255 },
-                    format:     { with: EMAIL_FORMAT },
-                    uniqueness: true
+  # before_save { email.downcase! }
+  # EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  # validates :email, presence:   true,
+  #                   length:     { maximum: 255 },
+  #                   format:     { with: EMAIL_FORMAT },
+  #                   uniqueness: true
 
-  has_secure_password
-  validates :password,  presence: true,
-                        length:   { minimum: 8 }
+  # has_secure_password
+  # validates :password,  presence: true,
+  #                       length:   { minimum: 8 }
 end
