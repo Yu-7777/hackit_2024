075ec0b94619f241972667cfd5d9c1f89bef6b28
Yu-7777 <c1306491@st.kanazawa-it.ac.jp>
2024-03-14 13:05:09 +0900

    バイトidの追加

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_14_031226) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_14_040225) do
   create_table "part_time_colors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "colorcode"
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_14_031226) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "up_manny"
+    t.integer "part_time_id"
   end
 
   create_table "shifts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
