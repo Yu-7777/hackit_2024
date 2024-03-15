cc28cafe64be9ded12f8f798ec532140a64c728e
Yu-7777 <c1306491@st.kanazawa-it.ac.jp>
2024-03-16 02:17:58 +0900

    gachaコントローラーのupdate追加、paramsの設定

--- back/app/controllers/gachas_controller.rb
+++ back/app/controllers/gachas_controller.rb
@@ -28,10 +28,13 @@ class GachasController < ApplicationController
 
   # PATCH/PUT /gachas/1
   def update
-    if @gacha.update(gacha_params)
-      render json: @gacha
+    user_id = current_api_v1_user
+    gacha = Gacha.find(params[:id])
+
+    if gacha.user_id == user_id && gacha.update(gach_params)
+      render json: gacha.to_json, status: :ok
     else
-      render json: @gacha.errors, status: :unprocessable_entity
+      render json: gacha.errors, status: :unprocessable_entity
     end
   end
 
@@ -41,13 +44,13 @@ class GachasController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_gacha
-      @gacha = Gacha.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
     def gacha_params
-      params.require(:gacha).permit(:title, :money, :count, :memo)
+      params.require(:gacha).permit(
+        :user_id,
+        :title,
+        :money,
+        :count,
+        :memo
+      )
     end
 end
