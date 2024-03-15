e7515f7aee47c8e72cbac82fa50a8de2683bbaaf
Yu-7777 <c1306491@st.kanazawa-it.ac.jp>
2024-03-16 01:48:05 +0900

    gachaのフォーマットの書くファイル間違えていたので修正

--- back/app/models/gacha.rb
+++ back/app/models/gacha.rb
@@ -1,2 +1,12 @@
 class Gacha < ApplicationRecord
+
+  def to_json
+    {
+      usrId: user_id,
+      title: title,
+      money: money,
+      count: count,
+      memo: memo
+    }
+  end
 end
--- back/app/models/part_time_color.rb
+++ back/app/models/part_time_color.rb
@@ -1,13 +1,3 @@
 class PartTimeColor < ApplicationRecord
   has_many :part_times, dependent: :destroy
-
-  def to_json
-    {
-      userId: user_id,
-      title: title,
-      money: money,
-      count: count,
-      memo: memo
-    }
-  end
 end
