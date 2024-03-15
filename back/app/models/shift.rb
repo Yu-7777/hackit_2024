306f406a7ea52a66267c609763223096cea19190
Yu-7777 <c1306491@st.kanazawa-it.ac.jp>
2024-03-15 13:15:11 +0900

    jsonのフォーマット整形

--- back/app/models/shift.rb
+++ back/app/models/shift.rb
@@ -1,3 +1,18 @@
 class Shift < ApplicationRecord
   belongs_to :parttime
+
+  def to_json
+    {
+      id: shift_id,
+      partTimes: {
+        id:   job_id,
+        name: job_name
+      }
+      title: shift_title,
+      start: work_start,
+      end: work_end,
+      restTime: rest_time,
+      memo: shift_memo
+    }
+  end
 end
