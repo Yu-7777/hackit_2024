43ce3fdef9e4297909053a84cea57acdbd4d8c28
Yu-7777 <c1306491@st.kanazawa-it.ac.jp>
2024-03-15 23:49:29 +0900

    gachaのフォーマット整形

--- back/app/models/part_time_color.rb
+++ back/app/models/part_time_color.rb
@@ -1,3 +1,13 @@
 class PartTimeColor < ApplicationRecord
   has_many :part_times, dependent: :destroy
+
+  def to_json
+    {
+      userId: user_id,
+      title: title,
+      money: money,
+      count: count,
+      memo: memo
+    }
+  end
 end
