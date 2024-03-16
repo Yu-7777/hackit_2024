fe7900eb95e8995e83d58753eed66b3330ac8fcd
YAMAKI-Kengo <c1318809@st.kanazawa-it.ac.jp>
2024-03-16 21:48:03 +0900

    ADD:サイドメニューの金額一覧の欄を作成

--- front/app/components/SalaryList.tsx
+++ front/app/components/SalaryList.tsx
@@ -7,15 +7,33 @@ const SalaryList = () => {
   return (
     <>
       <div className="ml-4 mt-8 text-2xl">給料一覧</div>
-      <Icons>
-        <BsCurrencyYen />
-      </Icons>
-      <Icons>
-        <BsCoin />
-      </Icons>
-      <Icons>
-        <BsPiggyBank />
-      </Icons>
+      <div className="flex items-center">
+        <Icons>
+          <BsCurrencyYen />
+        </Icons>
+        <div className="ml-4 rounded-md w-48 h-8 border-gray-300 justify-between">
+          <div>目標：</div>
+          <div>50000</div>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <Icons>
+          <BsCoin />
+        </Icons>
+        <div className="ml-4 rounded-md w-48 h-8 border-gray-300 justify-between">
+          <div>今日まで：</div>
+          <div>50000</div>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <Icons>
+          <BsPiggyBank />
+        </Icons>
+        <div className="ml-4 rounded-md w-48 h-8 border-gray-300 justify-between">
+          <div>見込：</div>
+          <div>50000</div>
+        </div>
+      </div>
     </>
   );
 };
