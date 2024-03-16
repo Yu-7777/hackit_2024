3bd8a9d212aac3bd5157b5885bee1ce14e55f79f
root <root@LAPTOP-LUAEL5UC>
2024-03-17 01:10:42 +0900

    modelの追加

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_15_165928) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_16_091403) do
   create_table "gachas", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.integer "money"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_15_165928) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "probability"
   end
 
   create_table "part_time_colors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -80,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_15_165928) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "goal_annual_income"
+    t.integer "goal_monthly_income"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
