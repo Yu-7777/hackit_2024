3030284aaaf4a216a322636de71fc9179644d741
elca-hub <csubaru07@gmail.com>
2024-03-18 13:50:07 +0900

    目標金額の設定を行うサイドピークの表示に関する不具合を修正

---
+++ front/app/components/Sidemenu/GoalMoneySideMenu.tsx
@@ -0,0 +1,58 @@
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { ReactElement } from "react";
+import SaveButton from "../SavaButton";
+
+type Props = {
+  data: {
+    isOpen: boolean,
+    onOpen: () => void,
+    onClose: () => void,
+    btnRef: React.RefObject<HTMLButtonElement>,
+  },
+  headerName: string,
+  saveButtonName: string,
+  children: React.ReactNode,
+};
+
+const GoalMoneySideMenu: React.FC<Props> = ({
+  data: { isOpen, onOpen, onClose, btnRef },
+  headerName,
+  saveButtonName,
+  children,
+}) => {
+
+  return (
+    <>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerContent>
+          <DrawerHeader className="flex justify-between items-center bg-gray-200">
+            <DrawerCloseButton />
+            {headerName}
+          </DrawerHeader>
+
+          <DrawerBody>{children}</DrawerBody>
+
+          <DrawerFooter>
+            <SaveButton buttonName={saveButtonName} />
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default GoalMoneySideMenu;
--- front/app/components/Sidemenu/SettingMoney.tsx
+++ front/app/components/Sidemenu/SettingMoney.tsx
@@ -2,6 +2,7 @@ import { Button, useDisclosure } from "@chakra-ui/react";
 import React from "react";
 import InputBox from "../inputBox";
 import Sidepeak from "../Sidepeak";
+import GoalMoneySideMenu from "./GoalMoneySideMenu";
 
 const SettingMoney = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -9,18 +10,19 @@ const SettingMoney = () => {
 
   return (
     <>
-      <Sidepeak
-        openResource={
-          <Button width="200px" colorScheme="white" textColor="black" ref={btnRef} onClick={onOpen}>
-            目標金額の変更
-          </Button>
-        }
+      <Button width="200px" colorScheme="white" textColor="black" ref={btnRef} onClick={onOpen}>
+        目標金額の変更
+      </Button>
+
+      <GoalMoneySideMenu
+        data={{ isOpen, onOpen, onClose, btnRef }}
         headerName={"目標の変更"}
+        saveButtonName={"保存"}
       >
         <InputBox round={""} inputName={"目標"}></InputBox>
         <InputBox round={""} inputName={"今日まで"}></InputBox>
         <InputBox round={""} inputName={"見込"}></InputBox>
-      </Sidepeak>
+      </GoalMoneySideMenu>
     </>
   );
 };
