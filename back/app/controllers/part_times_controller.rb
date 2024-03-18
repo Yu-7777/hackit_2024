406590d62bc1d2c533b7cafefbeb5b3b29089dc2
elca-hub <csubaru07@gmail.com>
2024-03-18 11:52:30 +0900

    partTimeのindexとshowの実装

--- back/app/controllers/part_times_controller.rb
+++ back/app/controllers/part_times_controller.rb
@@ -3,6 +3,7 @@ class PartTimesController < ApplicationController
     user_id = current_api_v1_user.id
     part_time = PartTime.new(part_time_params)
     part_time.user_id = user_id
+    part_time.part_time_color = PartTimeColor.find(1)
 
     if part_time.save
       render json: part_time, status: :created
@@ -17,6 +18,24 @@ class PartTimesController < ApplicationController
 
     if part_time.user_id == user_id && part_time.update(part_time_params)
       render json: part_time, status: :ok
+    else
+      render json: part_time.errors, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    user_id = current_api_v1_user.id
+    part_times = PartTime.where(user_id: user_id)
+
+    render json: part_times.map(&:index_part_times_to_json), status: :ok
+  end
+
+  def show
+    user_id = current_api_v1_user.id
+    part_time = PartTime.find(params[:id])
+
+    if user_id == part_time.user_id
+      render json: part_time.details_part_time_to_json, status: :ok
     else
       render json: part_time.errors, status: :unprocessable_entity
     end
