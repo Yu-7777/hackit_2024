5d022fde0e67f5994408e588dd64f29748a4fb29
elca-hub <csubaru07@gmail.com>
2024-03-18 13:22:00 +0900

    shiftモデルの不要な箇所を削除

--- back/app/models/shift.rb
+++ back/app/models/shift.rb
@@ -1,18 +1,3 @@
 class Shift < ApplicationRecord
   belongs_to :part_time
-
-  def to_json
-    {
-      id: shift_id,
-      partTimes: {
-        id:   part_time_id,
-        name: job_name
-      },
-      title: shift_title,
-      start: work_start,
-      end: work_end,
-      restTime: rest_time,
-      memo: shift_memo
-    }
-  end
 end
