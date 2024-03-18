858e91674f800bbf9f00ef330d0d3df059880413
elca-hub <csubaru07@gmail.com>
2024-03-18 13:21:07 +0900

    カレンダー情報を取得するAPIの内部処理を作成。ただし動作については保証しない。

--- back/app/controllers/calendars_controller.rb
+++ back/app/controllers/calendars_controller.rb
@@ -3,9 +3,33 @@ class CalendarsController < ApplicationController
 
   # GET /calendars
   def index
-    calendars = Calendar.all
+    user_id = current_api_v1_user.id
 
-    render json: calendars.to_json
+    # ユーザのバイト情報を取得
+    part_times = PartTime.where(user_id: user_id)
+    shifts = Shift.where(part_time_id: part_times.ids)
+
+    # カレンダーに表示するためのデータを作成
+    convert_shifts = []
+    shifts.each do |shift|
+      convert_shifts.push({
+        "id": shift.id,
+        "title": shift.shift_title,
+        "color": shift.part_time.part_time_color.colorcode,
+        "date": shift.work_start.strftime("%Y-%m-%d"),
+    })
+    end
+
+    json_data = {
+      calendar: [
+        {
+          shifts: shifts
+        }
+      ],
+      goalAnnualIncome: current_api_v1_user.goal_annual_income
+    }
+
+    render json: json_data
   end
 
   # GET /calendars/1
